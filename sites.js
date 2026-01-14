e1fc94f6ed42465e8dc8b40a2624d6ebecc08940
rkillam-thetrustees <robert.killam@thetrustees.org>
2026-01-14 11:22:13 -0500

    Create sites.js

---
+++ sites.js
@@ -0,0 +1,13 @@
+// Configuration file for monitoring sites
+// Each site has a center point and default zoom
+
+const sites = {
+  "Example Site 1": {
+    center: [42.3501, -71.1056],
+    zoom: 15
+  },
+  "Example Site 2": {
+    center: [42.4123, -71.2234],
+    zoom: 14
+  }
+};
