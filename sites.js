d14928c6a35911d7787e0ff9835c75898976f89c
rkillam-thetrustees <robert.killam@thetrustees.org>
2026-01-14 12:07:16 -0500

    Update sites.js

--- sites.js
+++ sites.js
@@ -2,12 +2,88 @@
 // Each site has a center point and default zoom
 
 const sites = {
-  "Example Site 1": {
-    center: [42.3501, -71.1056],
-    zoom: 15
+  "Allen C. Haskell Public Gardens": {
+    center: [41.65506297892783, -70.94590886990241],
+    zoom: 14
   },
-  "Example Site 2": {
-    center: [42.4123, -71.2234],
+  "Armstrong-Kelley Park": {
+    center: [41.62841480486134, -70.38034414659099],
+    zoom: 14
+  },
+  "Copicut Woods": {
+    center: [41.70242094669979, -71.0515022088127],
+    zoom: 14
+  },
+  "Cornell Farm": {
+    center: [41.555900026882846, -70.96106246911668],
+    zoom: 14
+  },
+  "East Over Reservation": {
+    center: [41.747965756075416, -70.81139299061721],
+    zoom: 14
+  },
+  "East Over: Hales Brook and Sippican River": {
+    center: [41.7396176529525, -70.78087923250978],
+    zoom: 14
+  },
+  "Eleanor Cabot Bradley Estate": {
+    center: [42.200349254121406, -71.12436733684274],
+    zoom: 14
+  },
+  "Francis William Bird Park": {
+    center: [42.15706910789175, -71.21629616203583],
+    zoom: 14
+  },
+  "Governor Oliver Ames Estate": {
+    center: [42.07036327686354, -71.09897794998899],
+    zoom: 14
+  },
+  "Holmes Reservation": {
+    center: [41.97021220891568, -70.6766225572128],
+    zoom: 14
+  },
+  "Lowell Holly": {
+    center: [41.66437003327789, -70.48049257614025],
+    zoom: 14
+  },
+  "Lyman Reserve": {
+    center: [41.764224796400754, -70.63380170730092],
+    zoom: 14
+  },
+  "Mashpee River Reservation": {
+    center: [41.62222414436522, -70.48030716856861],
+    zoom: 14
+  },
+  "Moose Hill Farm": {
+    center: [42.131265740545, -71.2128972316226],
+    zoom: 14
+  },
+  "Norris Reservation": {
+    center: [42.15583757030394, -70.78673193707866],
+    zoom: 14
+  },
+  "Signal Hill Reservation": {
+    center: [42.18752055975955, -71.1604686706001],
+    zoom: 14
+  },
+  "Slocum’s River Reserve": {
+    center: [41.552675043437475, -71.00669412046506],
+    zoom: 14
+  },
+  "Weir River Farm": {
+    center: [42.23793729075384, -70.85634117351793],
+    zoom: 14
+  },
+  "Westport Town Farm": {
+    center: [41.58000442761774, -71.07742205499062],
+    zoom: 14
+  },
+  "Whitney and Thayer Woods": {
+    center: [42.23190813345651, -70.83950609048523],
+    zoom: 14
+  },
+  "World's End": {
+    center: [42.26502546686153, -70.87470452174752],
     zoom: 14
   }
 };
